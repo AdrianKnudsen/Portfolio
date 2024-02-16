57379cfe037c699864bd7017ec2e2949ba06ca8e
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2024-02-16 12:37:48 +0100

    added hamburger nav

--- javascript/index.js
+++ javascript/index.js
@@ -46,3 +46,62 @@ document.addEventListener("DOMContentLoaded", () => {
   rootElement.appendChild(header);
   rootElement.appendChild(paragraph);
 });
+
+/// Nav Meny ///
+
+const elem = document.querySelector("#nav-bg"),
+  toggleBtn = document.querySelector("#toggle-btn"),
+  elemH = elem.getBoundingClientRect().height,
+  elemW = elem.getBoundingClientRect().width;
+
+let open = false;
+let scale, offsetX, offsetY;
+
+const calculateValues = () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  const offsetValue = Number(
+    getComputedStyle(elem).getPropertyValue("--offset-value")
+  );
+
+  //  Offsets to center the circle
+  offsetX = w / 2 - elemW / 2 - offsetValue;
+  offsetY = h / 2 - elemH / 2 - offsetValue;
+
+  const radius = Math.sqrt(h ** 2 + w ** 2);
+  scale = radius / (elemW / 4) / 3 + 0.3;
+  return scale;
+};
+
+const openMenu = () => {
+  elem.style.setProperty("--translate-x", `${offsetY}px`);
+  elem.style.setProperty("--translate-y", `-${offsetX}px`);
+  elem.style.setProperty("--scale", scale);
+};
+const closeMenu = () => {
+  elem.style.setProperty("--scale", 1);
+  elem.style.setProperty("--translate-x", 0);
+  elem.style.setProperty("--translate-y", 0);
+};
+const animateMenu = () => {
+  open ? openMenu() : closeMenu();
+};
+
+const toggleMenu = () => {
+  open = !open;
+  animateMenu();
+  toggleBtn.classList.toggle("shown");
+};
+
+const resizeHandler = () => {
+  window.requestAnimationFrame(() => {
+    calculateValues();
+    animateMenu();
+  });
+};
+
+calculateValues();
+
+//toggleBtn.onclick = toggleMenu;
+toggleBtn.addEventListener("click", toggleMenu, false);
+window.addEventListener("resize", resizeHandler, false);
